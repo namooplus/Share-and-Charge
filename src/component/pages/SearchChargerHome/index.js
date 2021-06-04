5e7b3742b2742e20a754f55a661009675dec47b3
namoo <namoo.developer@gmail.com>
2021-06-04 18:33:31 +0900

    Add loading state in fetching chargers

--- src/component/pages/SearchChargerHome/index.js
+++ src/component/pages/SearchChargerHome/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { BaseLayout, HeaderLayout, MenuLayout, MenuIcon, HeaderLabel, ContentLayout } from './components';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,10 +7,40 @@ import ShadowSearch from '../../common/ShadowSearch';
 import ShadowCard from '../../common/ShadowCard';
 import AppIcon from '../../common/AppIcon';
 
+import axios from 'axios';
+
 import tempChargerImage from '../../../img/temp-charger-image.png';
 
 function SearchChargerHome(props) {
-    
+    const [chargerList, setChargerList] = useState([0]);
+
+    const successGet = (position) => {
+        // 주변의 공유 충전소 리스트 요청
+        // axios.post('./tempCharger.json', {
+        //     latitude: position.coords.latitude,
+        //     longitude: position.coords.longitude
+        // })
+        axios.get('./tempData/chargerList.json')
+        // 주변의 공유 충전소 리스트 출력
+        .then(res => {
+            setChargerList(res.data);
+        })
+        .catch(err => {
+            setChargerList([1]);
+        });
+    };
+    const failGet = () => {
+        setChargerList([1]);
+    };
+
+    useEffect(() => {
+        // 현재 위치 찾기
+        if (navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(successGet, failGet);
+        else
+            setChargerList([1]);
+    }, []);
+
     return (
         <BaseLayout>
             <HeaderLayout>
@@ -22,24 +52,21 @@ function SearchChargerHome(props) {
             </HeaderLayout>
             <ContentLayout>
                 <ShadowSearch placeholder="원하는 지역의 공유 주차장을 입력하세요"/>
-                <ShadowCard
-                    chargerImage={tempChargerImage}
-                    chargerTitle="충전소 A"
-                    chargerDescription="15km | 시간 당 1650원 | 13:00~18:00"
-                    chargerUserIcon
-                    chargerUserLabel="User A"/>
-                <ShadowCard 
-                    chargerImage={tempChargerImage}
-                    chargerTitle="충전소 B"
-                    chargerDescription="15km | 시간 당 1760원 | 12:00~15:00"
-                    chargerUserIcon
-                    chargerUserLabel="User B"/>
-                <ShadowCard
-                    chargerImage={tempChargerImage}
-                    chargerTitle="충전소 C"
-                    chargerDescription="15km | 시간 당 1760원 | 12:00~15:00"
-                    chargerUserIcon
-                    chargerUserLabel="User C"/>
+                {chargerList.map((data) => {
+                    if (data === 0)
+                        return <div style={{textAlign: 'center'}}>불러오는 중...</div>
+                    else if (data === 1)
+                        return <div style={{textAlign: 'center'}}>오류 : 주변의 공유 충전소를 불러올 수 없습니다!</div>
+                    else
+                        return (
+                            <ShadowCard
+                            chargerImage={tempChargerImage}
+                            chargerTitle={data.name}
+                            chargerDescription={`${data.distance} | ${data.price} | ${data.time}`}
+                            chargerUserIcon
+                            chargerUserLabel={data.share}/>
+                        )
+                })}
             </ContentLayout>
         </BaseLayout>
     );
