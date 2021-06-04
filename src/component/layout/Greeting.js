3b7c5cb465ecc5a9357cf8465298665ead6d95e7
kyle8581 <mapoout@naver.com>
2021-06-04 16:29:35 +0900

    login

--- src/component/layout/Greeting.js
+++ src/component/layout/Greeting.js
@@ -2,17 +2,30 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
-
+import {GoogleLogin} from "react-google-login"
 import ShadowButton from '../view/ShadowButton';
 import AppIcon from '../view/AppIcon';
+// client pw :_WeZKKlPuJQouGS1R0Jm2pP3
+const responseGoogle = (response) =>{
+    console.log(response);
+    console.log(response.profileObj);
 
+}
 function Greeting(props) {
+    
   return (
     <BaseLayout>
         <SplashLayout>
             <AppIcon width="80px" height="80px"/>
             <AppLabel>Share & Charge</AppLabel>
         </SplashLayout>
+        <GoogleLogin
+            clientId="16778233937-f1jvjb9bsru3kjgsri1b9oojg2qvbie2.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={"single_host_origin"}
+        />
         <ShadowButton><FontAwesomeIcon icon={faGoogle}/>&nbsp;&nbsp;구글 계정으로 로그인</ShadowButton>
     </BaseLayout>
   );
--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+document.cookie = "safeCookie1=foo; SameSite=Lax"; 
+document.cookie = "safeCookie2=foo"; 
+document.cookie = "crossCookie=bar; SameSite=None; Secure";
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
