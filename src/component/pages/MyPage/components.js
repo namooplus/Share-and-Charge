0ffa634b0701681ebecdcab048fd9278367fab51
kyle8581 <mapoout@naver.com>
2021-06-05 15:15:34 +0900

    fetch user data

---
+++ src/component/pages/MyPage/components.js
@@ -0,0 +1,28 @@
+import styled from "styled-components/macro";
+
+export const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  padding: 2rem;
+  flex-direction: column;
+  
+`;
+export const Profile = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 1rem;
+`;
+export const Credit = styled.div`
+  font-size: 3rem;
+  margin-left: 4rem;
+`;
+export const UserImage = styled.img`
+    width : 5rem;
+    border-radius: 5rem;
+`;
+export const UserName = styled.div`
+    font-size: 1.5rem;
+
+`
