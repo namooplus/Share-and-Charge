b7af516f224da9b0fcbc9428e2738eb197894f4c
kyle8581 <mapoout@naver.com>
2021-06-07 01:20:29 +0900

    sendbird ui

--- src/component/pages/ChattingRoom/Chat/Index.js
+++ src/component/pages/ChattingRoom/Chat/Index.js
@@ -3,12 +3,12 @@ import "./index.css";
 import React, { useEffect, useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import {
-    SendBirdProvider,
-    ChannelList,
-    Channel,
-    ChannelSettings,
-    withSendBird,
-    sendBirdSelectors,
+  SendBirdProvider,
+  ChannelList,
+  Channel,
+  ChannelSettings,
+  withSendBird,
+  sendBirdSelectors,
 } from "sendbird-uikit";
 import "sendbird-uikit/dist/index.css";
 import axios from "axios";
@@ -20,136 +20,131 @@ import { ViewContext } from "./ViewContext";
 const channelUrl = process.env.CHANNEL_URL;
 
 const CustomComponent = ({
-    createChannel,
-    sdk,
-    leaveChannel,
-    userId,
-    providerId,
+  createChannel,
+  sdk,
+  leaveChannel,
+  userId,
+  providerId,
 }) => {
-    const [channelUrl, setChannelUrl] = useState("");
+  const [channelUrl, setChannelUrl] = useState("");
+  const [show, setShow] = useState(true);
 
-    return (
-        <>
-            <button
-                onClick={() => {
-                    const curl = userId + "_" + providerId;
+  return (
+    show?
+    <>
+    <div className="create_channel_bg">
 
-                    let params = new sdk.GroupChannelParams();
-                    params.isPublic = false;
-                    params.isEphemeral = false;
-                    params.isDistinct = false;
-                    params.addUserIds([providerId, "mapoout@yonsei.ac.kr"]);
-                    params.name = curl;
-                    createChannel(params)
-                        .then((c) => {
-                            console.log(curl);
-                            setChannelUrl(curl);
-                        })
-                        .catch((c) => console.warn(c));
-                }}
-            >
-                {" "}
-                Create channel
-            </button>
-            <button
-                onClick={() => {
-                    leaveChannel(channelUrl)
-                        .then((c) => {
-                            setChannelUrl("");
-                        })
-                        .catch((c) => console.warn(c));
-                }}
-            >
-                {" "}
-                Leave channel
-            </button>
-            <br />
-            {`Created channel is: ${channelUrl}`}
-        </>
-    );
+      <div className="create_channel_box">
+        <div className="create_channel_message">
+         <p className="provider_id">{"mapoout@naver.com"}</p>  <p>님과 채팅을 시작합니다</p>
+        </div>
+        <button
+          className="create_channel_button"
+          onClick={() => {
+            const curl = userId + "_" + providerId;
+
+            let params = new sdk.GroupChannelParams();
+            params.isPublic = false;
+            params.isEphemeral = false;
+            params.isDistinct = false;
+            params.addUserIds([providerId, "mapoout@yonsei.ac.kr"]);
+            params.name = curl;
+            createChannel(params)
+              .then((c) => {
+                console.log(curl);
+                setChannelUrl(curl);
+              })
+              .catch((c) => console.warn(c));
+            setShow(false);
+          }}
+        >
+          {" "}
+          Create channel
+        </button>
+      </div>
+    </div>
+
+     
+    </> : <></>
+  );
 };
 
 const CustomComponentWithSendBird = withSendBird(CustomComponent, (state) => {
-    const createChannel = sendBirdSelectors.getCreateChannel(state);
-    const leaveChannel = sendBirdSelectors.getLeaveChannel(state);
-    const sdk = sendBirdSelectors.getSdk(state);
-    const userId = localStorage.getItem("username");
-    const providerId = localStorage.getItem("providerId");
-    return { createChannel, sdk, leaveChannel, userId, providerId };
+  const createChannel = sendBirdSelectors.getCreateChannel(state);
+  const leaveChannel = sendBirdSelectors.getLeaveChannel(state);
+  const sdk = sendBirdSelectors.getSdk(state);
+  const userId = localStorage.getItem("username");
+  const providerId = localStorage.getItem("providerId");
+  return { createChannel, sdk, leaveChannel, userId, providerId };
 });
 export default function Chat({
-    userId,
-    theme,
-    nickname,
-    useCustomQuery,
-    providerId,
-    toCreate,
+  userId,
+  theme,
+  nickname,
+  useCustomQuery,
+  providerId,
+  toCreate,
 }) {
-    const [showSettings, setShowSettings] = useState(false);
-    const [currentChannelUrl, setCurrentChannelUrl] = useState(null);
-    console.log(ViewContext);
-    console.log(useContext(ViewContext));
-    const { view, toggleview } = useContext(ViewContext);
-    const changeView = () => {
-        if (view) toggleview(false);
-        else toggleview(true);
-    };
-    return (
-        <div style={{ height: "100vh" }}>
-            <SendBirdProvider
-                appId={"261DB632-487D-4E3C-922F-6D453A33B82D"}
-                theme={theme}
-                userId={userId}
-                nickname={nickname}
-                userListQuery={useCustomQuery ? getCustomPaginatedQuery : null}
-            >
-                <div className="sendbird-app__wrap">
-                    <div className="sendbird-app__channellist-wrap">
-                        {toCreate ? <CustomComponentWithSendBird /> : <></>}
+  const [showSettings, setShowSettings] = useState(false);
+  const [currentChannelUrl, setCurrentChannelUrl] = useState(null);
+  console.log(ViewContext);
+  console.log(useContext(ViewContext));
+  const { view, toggleview } = useContext(ViewContext);
+  const changeView = () => {
+    if (view) toggleview(false);
+    else toggleview(true);
+  };
+  return (
+    <div className="sendbird_wrapper">
+      <SendBirdProvider
+        appId={"261DB632-487D-4E3C-922F-6D453A33B82D"}
+        theme={theme}
+        userId={userId}
+        nickname={nickname}
+        userListQuery={useCustomQuery ? getCustomPaginatedQuery : null}
+      >
+        <div className="sendbird-app__wrap">
+          <div className="sendbird-app__channellist-wrap">
+            {toCreate ? <CustomComponentWithSendBird /> : <></>}
 
-                        <ChannelList
-                            renderChannelPreview={ChannelPreview}
-                            onChannelSelect={(channel) => {
-                                if (channel && channel.url) {
-                                    setCurrentChannelUrl(channel.url);
-                                }
-                            }}
-                        />
-                        <div>{providerId}</div>
-                    </div>
-                    <div
-                        className="sendbird-app__conversation-wrap"
-                        style={{
-                            transform: view
-                                ? "translateX(0)"
-                                : "translateX(-100vw)",
-                        }}
-                    >
-                        <button
-                            onClick={changeView}
-                            className="tolist"
-                        ></button>
+            <ChannelList
+              renderChannelPreview={ChannelPreview}
+              onChannelSelect={(channel) => {
+                if (channel && channel.url) {
+                  setCurrentChannelUrl(channel.url);
+                }
+              }}
+            />
+            <div>{providerId}</div>
+          </div>
+          <div
+            className="sendbird-app__conversation-wrap"
+            style={{
+              transform: view ? "translateX(0)" : "translateX(-100vw)",
+            }}
+          >
+            <button onClick={changeView} className="tolist"></button>
 
-                        <Channel
-                            renderChatItem={Message}
-                            channelUrl={currentChannelUrl}
-                            onChatHeaderActionClick={() => {
-                                setShowSettings(true);
-                            }}
-                        />
-                    </div>
-                </div>
-                {showSettings && (
-                    <div className="sendbird-app__settingspanel-wrap">
-                        <ChannelSettings
-                            channelUrl={currentChannelUrl}
-                            onCloseClick={() => {
-                                setShowSettings(false);
-                            }}
-                        />
-                    </div>
-                )}
-            </SendBirdProvider>
+            <Channel
+              renderChatItem={Message}
+              channelUrl={currentChannelUrl}
+              onChatHeaderActionClick={() => {
+                setShowSettings(true);
+              }}
+            />
+          </div>
         </div>
-    );
+        {showSettings && (
+          <div className="sendbird-app__settingspanel-wrap">
+            <ChannelSettings
+              channelUrl={currentChannelUrl}
+              onCloseClick={() => {
+                setShowSettings(false);
+              }}
+            />
+          </div>
+        )}
+      </SendBirdProvider>
+    </div>
+  );
 }
