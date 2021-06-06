64674fadeb2ea90d509dd34cceee9bc66ebf0821
kyle8581 <mapoout@naver.com>
2021-06-07 00:38:16 +0900

    login process

--- src/component/pages/RegisterUser/index.js
+++ src/component/pages/RegisterUser/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -13,129 +13,132 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { UserContext } from "../context/UserNameContext";
+import axios from "axios";
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: "100%", // Fix IE 11 issue.
-        marginTop: theme.spacing(3),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 function RegisterUser({ setLoginServer }) {
-    const classes = useStyles();
-    const { username } = useContext(UserContext);
+  const classes = useStyles();
+  const { username } = useContext(UserContext);
+  const [englishName, setEnglishName] = useState("");
+  const [phoneNumner, setPhoneNumber] = useState("");
 
-    const [englishName, setEnglishName] = useState("");
-    const [phoneNumner, setPhoneNumber] = useState("");
+  const handleNameChange = (e) => {
+    setEnglishName(e.target.value);
+  };
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+  };
 
-    const handleNameChange = (e) => {
-        setEnglishName(e.target.value);
-    };
-    const handlePhoneNumberChange = (e) => {
-        setPhoneNumber(e.target.value);
-    };
+ 
+  const registerSubmit = () => {
+    setLoginServer(true);
+	if(localStorage.getItem("temp_username")!==null){
+		localStorage.setItem("username", localStorage.getItem("temp_username"));
+		// localStorage.removeItem("temp_username");
 
-    const registerSubmit = () => {
-        setLoginServer(true);
-        localStorage.setItem("username", localStorage.getItem("temp_username"));
-        localStorage.removeItem("temp_username");
-    };
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
-                </Typography>
-                <form className={classes.form} noValidate>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                autoComplete="fname"
-                                name="firstName"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label="Name"
-                                autoFocus
-                                onChange={handleNameChange}
-                            />
-                        </Grid>
+	}
+	axios.get("http://1d829d6cab88.ngrok.io/signup/"+englishName.replace(/\s/g, '')+"/"+localStorage.getItem("username")+"/"+phoneNumner+"/"+localStorage.getItem("token")).then(
+		((res)=>{console.log(JSON.stringify(res.data))})
+	)
 
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                disabled
-                                id="email"
-                                label={username}
-                                name="email"
-                                autoComplete="email"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label={"Phone number"}
-                                name="phone_number"
-                                autoComplete="phone_number"
-                                onChange={handlePhoneNumberChange}
-                            />
-                        </Grid>
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="Name"
+                autoFocus
+                onChange={handleNameChange}
+              />
+            </Grid>
 
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        value="allowExtraEmails"
-                                        color="primary"
-                                    />
-                                }
-                                label="I want to receive inspiration, marketing promotions and updates via email."
-                            />
-                        </Grid>
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                        onClick={registerSubmit}
-                    >
-                        Sign Up
-                    </Button>
-                    <Grid container justify="flex-end">
-                        <Grid item>
-                            <Link href="/" variant="body2">
-                                Already have an account? Sign in
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-            <Box mt={5}></Box>
-        </Container>
-    );
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                disabled
+                id="email"
+                label={localStorage.getItem("username")}
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label={"Phone number"}
+                name="phone_number"
+                autoComplete="phone_number"
+                onChange={handlePhoneNumberChange}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={registerSubmit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}></Box>
+    </Container>
+  );
 }
 
 export default RegisterUser;
